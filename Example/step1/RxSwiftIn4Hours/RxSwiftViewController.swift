3bcbfeaa86e99d22f23c24fe3e4c21b762160451
국윤수 <yunsu.guk@wadiz.kr>
2019-07-25 22:43:21 +0900

    Step1: Observable, DIsposable

--- Example/step1/RxSwiftIn4Hours/RxSwiftViewController.swift
+++ Example/step1/RxSwiftIn4Hours/RxSwiftViewController.swift
@@ -10,57 +10,58 @@ import RxSwift
 import UIKit
 
 class RxSwiftViewController: UIViewController {
-    // MARK: - Field
+  // MARK: - Field
 
-    var counter: Int = 0
+  var counter: Int = 0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
-            self.counter += 1
-            self.countLabel.text = "\(self.counter)"
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+      self.counter += 1
+      self.countLabel.text = "\(self.counter)"
+    }
+  }
+
+  // MARK: - IBOutlet
+
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var countLabel: UILabel!
+
+  // MARK: - IBAction
+  var disposeBag: DisposeBag = DisposeBag()
+
+  @IBAction func onLoadImage(_ sender: Any) {
+    imageView.image = nil
+
+    rxswiftLoadImage(from: LARGER_IMAGE_URL)
+      .observeOn(MainScheduler.instance)
+      .subscribe({ result in
+        switch result {
+        case let .next(image):
+          self.imageView.image = image
+
+        case let .error(err):
+          print(err.localizedDescription)
+
+        case .completed:
+          break
         }
+      }).disposed(by: disposeBag)
+  }
+
+  @IBAction func onCancel(_ sender: Any) {
+    disposeBag = DisposeBag()
+  }
+
+  // MARK: - RxSwift
+
+  func rxswiftLoadImage(from imageUrl: String) -> Observable<UIImage?> {
+    return Observable.create { seal in
+      asyncLoadImage(from: imageUrl) { image in
+        seal.onNext(image)
+        seal.onCompleted()
+      }
+      return Disposables.create()
     }
-
-    // MARK: - IBOutlet
-
-    @IBOutlet var imageView: UIImageView!
-    @IBOutlet var countLabel: UILabel!
-
-    // MARK: - IBAction
-
-    @IBAction func onLoadImage(_ sender: Any) {
-        imageView.image = nil
-
-        _ = rxswiftLoadImage(from: LARGER_IMAGE_URL)
-            .observeOn(MainScheduler.instance)
-            .subscribe({ result in
-                switch result {
-                case let .next(image):
-                    self.imageView.image = image
-
-                case let .error(err):
-                    print(err.localizedDescription)
-
-                case .completed:
-                    break
-                }
-            })
-    }
-
-    @IBAction func onCancel(_ sender: Any) {
-        // TODO: cancel image loading
-    }
-
-    // MARK: - RxSwift
-
-    func rxswiftLoadImage(from imageUrl: String) -> Observable<UIImage?> {
-        return Observable.create { seal in
-            asyncLoadImage(from: imageUrl) { image in
-                seal.onNext(image)
-                seal.onCompleted()
-            }
-            return Disposables.create()
-        }
-    }
+  }
 }
